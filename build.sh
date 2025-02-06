3150fe001854677ad3e8d7032153bb8ccb48dd1c
Scott Faria <scott@faria.cx>
2025-02-06 20:54:26 +0100

    Change to thomasweise/texlive due to font rendering issues.

--- build.sh
+++ build.sh
@@ -4,10 +4,4 @@ echo "Cleaning old dist."
 make clean
 
 echo "Building..."
-docker run \
-    --rm \
-    --user $(id -u):$(id -g) \
-    -i \
-    -w "/doc" \
-    -v "$PWD":/doc \
-    texlive/texlive make
+docker run --rm --user $(id -u):$(id -g) -i -w "/doc" -v "$PWD":/doc thomasweise/texlive make
